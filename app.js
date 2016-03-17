4ea5ced00f54dabbda0142ffc7a4e3888b4f65af
Michael Douglas <mmoreira.dg@gmail.com>
2016-03-17 00:26:11 -0400

    Comiti iniciante

--- app.js
+++ app.js
@@ -1,15 +1,71 @@
 var app = angular.module('firstAppAngular', []);
 
-app.controller('divControll', function($scope){
+var DivControll = function($scope){
 
 	$scope.nome = 'Michael Douglas';
 
-	var OlaControll = function($scope){
+	$scope.lerNome = function(){
+		return $scope.nome;
+	}
+}
 
-		$scope.lerNome = function(){
-			return $scope.nome;
-		}
+var PaisesControll = function($scope){
+
+	$scope.populacao = 7000;
+
+	$scope.paises = [
+		{nome:'Brasil', populacao: 100},
+		{nome:'Alemanha', populacao: 700}
+	]
+}
+
+var TarefasControll = function($scope){
+
+
+	$scope.todos = [
+
+		{
+			tarefa:'Tarefa 01',
+			feito:true
+		},
+		{
+			tarefa:'Tarefa 02',
+			feito:false
+		},
+		{
+			tarefa:'Tarefa 03',
+			feito:false
+		},
+		{
+			tarefa:'Tarefa 04',
+			feito:false
+		},
+
+	];
+
+
+	//Adicionar novos elementos a lista
+	$scope.addTodo = function(){
+
+		$scope.todos.push({tarefa:$scope.todoText, feito:false});
+		$scope.todoText = '';
 
 	}
 
-});
+
+	//Calcular quantas tarefas estão prontas
+	$scope.restante = function(){
+
+		var count = 0;
+
+		angular.forEach($scope.todos, function(todo){
+			count += todo.feito ? 0 : 1;
+		});
+
+		return count;
+	}
+}
+
+app.controller('DivControll', DivControll);
+app.controller('PaisesControll', PaisesControll);
+app.controller('TarefasControll', TarefasControll);
