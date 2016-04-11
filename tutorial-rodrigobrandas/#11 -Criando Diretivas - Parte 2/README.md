c011cd2319fd45f85f598fb4764c83c70c948e17
Michael Douglas <mmoreira.dg@gmail.com>
2016-04-11 12:53:46 -0400

    criando diretivas parte 2

---
+++ tutorial-rodrigobrandas/#11 -Criando Diretivas - Parte 2/README.md
@@ -0,0 +1,6 @@
+O capitulo ensinou sobre:
+
+Criação de diretivas.
+
+Neste etapa será criada uma diretiva para realizar uma máscara de data.
+Ex: 10/10/2016
---
+++ tutorial-rodrigobrandas/#11 -Criando Diretivas - Parte 2/js/diretivas/mascDateDiretiva.js
@@ -0,0 +1,10 @@
+angular.module("listaTelefonicaApp").directive("uiMasckDate", function(){
+
+    return {
+
+      link: function(scope, element, attrs, controller){
+          console.log(controller);
+      }
+
+    };
+});
