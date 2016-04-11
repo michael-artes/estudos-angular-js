e81728a6e1d2ad042f0f0b54fcb439769e22e9ee
michael-artes <michael.artes@outlook.com>
2016-04-11 18:09:23 -0400

    parte 2 - criadao de diretivas

--- tutorial-rodrigobrandas/#11 -Criando Diretivas - Parte 2/README.md
+++ tutorial-rodrigobrandas/#11 -Criando Diretivas - Parte 2/README.md
@@ -4,3 +4,13 @@ Criação de diretivas.
 
 Neste etapa será criada uma diretiva para realizar uma máscara de data.
 Ex: 10/10/2016
+
+
+link: Executada depois do template ter sido compilado, podemos utiliza-la para
+interagir com a DOM, tratando eventos ou mesmo para definir o comportamento
+associado com a lógica da diretiva
+
+require: Estabelece um vínculo com outra diretiva, interagindo por meio do
+controller, que é um dos parâmetros da função link.
+
+transclude: É possível encapsular elementos dentro de uma diretiva
--- tutorial-rodrigobrandas/#11 -Criando Diretivas - Parte 2/js/diretivas/mascDateDiretiva.js
+++ tutorial-rodrigobrandas/#11 -Criando Diretivas - Parte 2/js/diretivas/mascDateDiretiva.js
@@ -1,10 +1,35 @@
-angular.module("listaTelefonicaApp").directive("uiMasckDate", function(){
+angular.module("listaTelefonicaApp").directive("uiMasckDate", function($filter){
 
-    return {
+  return {
+		require: "ngModel",
+		link: function (scope, element, attrs, ctrl) {
 
-      link: function(scope, element, attrs, controller){
-          console.log(controller);
-      }
+			var _formatDate = function (date) {
+				date = date.replace(/[^0-9]+/g, "");
+				if(date.length > 2) {
+					date = date.substring(0,2) + "/" + date.substring(2);
+				}
+				if(date.length > 5) {
+					date = date.substring(0,5) + "/" + date.substring(5,9);
+				}
+				return date;
+			};
 
-    };
+			element.bind("keyup", function () {
+				ctrl.$setViewValue(_formatDate(ctrl.$viewValue));
+				ctrl.$render();
+			});
+
+			ctrl.$parsers.push(function (value) {
+				if (value.length === 10) {
+					var dateArray = value.split("/");
+					return new Date(dateArray[2], dateArray[1]-1, dateArray[0]).getTime();
+				}
+			});
+
+			ctrl.$formatters.push(function (value) {
+				return $filter("date")(value, "dd/MM/yyyy");
+			});
+		}
+	};
 });
